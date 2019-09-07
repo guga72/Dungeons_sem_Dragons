36aecf1f76ffb4dc7a71b14f3ffc662d8fab1624
tiagoMurer <tiagostaurenghi@hotmail.com>
2019-09-07 13:50:59 -0300

    07.09.2019: Mais uns testes da classe Usuario.js

--- js/Usuario.js
+++ js/Usuario.js
@@ -1,10 +1,20 @@
 class Usuario {
-    constructor(nome) {
+    constructor(nome, id, senha) {
         this.nome = nome;
+        this.id = id;
+        this.senha = senha;
     }
     
     getNome(){
         return this.nome;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    getSenha(){
+        return this.senha;
     }
 }
 
--- js/Usuario.test.js
+++ js/Usuario.test.js
@@ -4,3 +4,13 @@ test('usuario deve ter um nome', () => {
     const usuario = new Usuario('Fujiro Kakombi');
     expect(usuario.getNome()).toBe('Fujiro Kakombi');
 });
+
+test('usuario deve ter id', () => {
+    const usuario = new Usuario('Ayres', 'xxPussySlayerxx');
+    expect(usuario.getId()).toBe('xxPussySlayerxx');
+});
+
+test('usuario deve ter senha', () => {
+    const usuario = new Usuario('Oskar', 'BridgeBuildah', 'gobuildabridge');
+    expect(usuario.getSenha()).toBe('gobuildabridge');
+});

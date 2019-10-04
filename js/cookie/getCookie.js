385bfce1554808005a282c91f00f5a18f208c74b
Pedrofreitas18 <opedrofreitas@gmail.com>
2019-10-04 02:15:00 -0200

    04/10 cookie

---
+++ js/cookie/getCookie.js
@@ -0,0 +1,10 @@
+function getCookie() {
+    return document.cookie;
+}
+
+function getCookieToString() {
+    return document.cookie.split("=");
+}
+
+//module.exports = getCookieToString;
+//module.exports = getCookie;

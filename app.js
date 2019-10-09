7cb17344e80abe385ecbf864b60a14a54551232e
guga72 <guga.72@hotmail.com>
2019-10-09 11:34:02 -0300

    09-10-2019 mudanças no metodo post

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-
 const Usuario = require('./js/Usuario.js');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -21,11 +22,20 @@ app.get('/', function(req, res) {
 });
 
 app.post('/paginas/cadastro.html', function(req, res) {
-    var senha2 = req.params.senha2;
-    var senha = req.params.senha;
-    var email = req.params.email;
-    const users = new Usuario('teste', 1, senha, email);
-    res.redirect("../index.html");
+    var teste = req.body;
+    console.log(teste);
+    console.log(teste.senha);
+    var senha2 =teste.confirmarsenha;
+    var senha = teste.senha;
+    var email = teste.email;
+    if (senha2 == senha){
+        const users = new Usuario('teste', 1, senha, email);
+        res.redirect("../index.html");
+    }
+    else{
+        app.use(logErrors);
+        //res.send({error:'Fudeu'});
+    }
 });
 
 app.listen(port, function() {

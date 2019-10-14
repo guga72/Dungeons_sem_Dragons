a8021554ce8da6b3959ad6659768d8c15fdba861
guga72 <guga.72@hotmail.com>
2019-10-14 16:06:09 -0300

    14-10-2019 criação do cookie

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
-const cookie = require('./js/cookie/cookie.js');
+var cookieParser = require('cookie-parser')
+//const cookie = require('./js/cookie/cookie.js');
 var app = express();
+app.use(cookieParser());
 
 const Usuario = require('./js/Usuario.js'); // pedindo requisição da classe usuario
 
@@ -8,18 +10,18 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 //para pegar as informações do corpo da aplicação (pegar os dados do form)
 
-var mysql = require('mysql'); // começo da comunicação com o banco de dados
+/*var mysql = require('mysql'); // começo da comunicação com o banco de dados
 var connetion = mysql.createConnection({ // criando a comunicação com o
     host: 'localhost',                   // banco
     user: 'root',
     password: '123456' 
-});
+});*/
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
-connetion.connect();
+//connetion.connect();
 
 app.get('/', function(req, res) {
     res.sendFile('index.html');
@@ -29,11 +31,19 @@ app.post('/paginas/cadastro.html', function(req, res) {
     var corpo = req.body;
     var senha = corpo.senha;
     var email = corpo.email;
-    console.log(req.body);
+    var cookie = req.cookies.email;
+    if (cookie === undefined){
+        res.cookie("email",email, { maxAge: 100000, httpOnly: true });
+        console.log('cookie criado');
+
+    }
+    else{
+        console.log('cookie existente');
+    }
     const users = new Usuario('teste', senha, email);
     //cookie.setCookie("email",corpo.email,2);
     //res.cookie="email="+corpo.email+"; path=/";
-    res.cookie("email", corpo.email);
+    //res.cookie("email", corpo.email);
     res.redirect("./game-page.html");
 });
 
@@ -51,4 +61,4 @@ app.listen(port, function() {
 });
 
 
-connetion.end();
+//connetion.end();

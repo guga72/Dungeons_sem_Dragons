87191cb78b8c348c16392ffd13fa7e3747590210
guga72 <guga.72@hotmail.com>
2019-11-08 12:46:25 -0200

    mudança no banco para o mysql

--- sql/DsemD.sql
+++ sql/DsemD.sql
@@ -1,35 +1,33 @@
+create database DsemD;
+use DsemD;
 create table equipamentos(
-       tipo_equipamento varchar2(25), -- machado, espada, escudo, cajado, adaga
-       atributo number, -- atributos de atq, defesa, ou qualquer outro que terá que ser ligado com outra tabela
-       nome_equi varchar2(25) primary key, -- nome do equipamento
-       descricao description, -- descrição previa da arma
-       foreign key (atributo) references atributo_arma(atri_id)
+       tipo_equipamento varchar(25), -- machado, espada, escudo, cajado, adaga
+       atributo int, -- atributos de atq, defesa, ou qualquer outro que terï¿½ que ser ligado com outra tabela
+       nome_equi varchar(25) primary key, -- nome do equipamento
+       descricao varchar(255), -- descriï¿½ï¿½o previa da arma
+       constraint fk_atributo foreign key (atributo) references atributo_arma(atri_id)
 );
 
 create table atributo_arma(
-       atri_id   number,
-       ataque    number, -- quantidade de ataque que vai dar a arma
-       defesa    number, -- queantidade de defesa
-       magia     number, -- quantidade de dano magico que a arma da
-       estado    number, -- a vida util da arma, quantidade de vida que ela tem(opcional)
+       atri_id   int primary key,
+       ataque    int, -- quantidade de ataque que vai dar a arma
+       defesa    int, -- queantidade de defesa
+       magia     int, -- quantidade de dano magico que a arma da
+       estado    int -- a vida util da arma, quantidade de vida que ela tem(opcional)
        
 );
 
 CREATE TABLE personagem(
-       vida NUMBER(10),
-       nome VARCHAR2(100),
-       sexo VARCHAR2(10),
-       raca VARCHAR2(50), -- isso vai ter uma tabela so para isso
+		id_personagem int primary key,
+       vida int,
+       nome VARCHAR(100),
+       sexo VARCHAR(10),
+       raca VARCHAR(50), -- isso vai ter uma tabela so para isso
        leel INTEGER,
-       gold NUMBER(20,2),
-       potions NUMBER(2),
-       atributos NUMBER(3),
-       ataques NUMBER(3), -- tabela
-       itens NUMBER(10), -- tabela
-       quest NUMBER(10) -- tabela
-);
-
-CREATE TABLE raca(
-
-
+       gold float,
+       potions float,
+       atributos int,
+       ataques int,-- tabela
+       itens int, -- tabela
+       quest int -- tabela
 );

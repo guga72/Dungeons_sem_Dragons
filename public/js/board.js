81f69ceaea5a96ed0ca230116cf8c89fee08879b
Pedrofreitas18 <opedrofreitas@gmail.com>
2019-11-08 11:48:36 -0200

    React modificacoes - 8/11

--- public/js/board.js
+++ public/js/board.js
@@ -11,17 +11,24 @@ class LikeButton extends React.Component {
       this.changeTelaState = this.changeTelaState.bind(this);
     }
 
-    changeTelaState() {
+    changeTelaState(e) {
+
       this.setState({ tela: 'Trono_Castelo_Pyton-Comeco_do_jogo' })
     }
   
+    handleChange(e) {
+      this.setState({ value: e.target.value });
+    } 
+
     render() {
       if (this.state.tela == 'Castelo_Phyton-Comeco_do_jogo') {
         return (
           <center>
             <h1>Tela State = {this.state.tela}</h1>
              <img src="../imagens/Castelo_Phyton_da_Colina.jpg" /> 
-             <button onClick={this.changeTelaState} >test</button>
+             <input defaultValue="0" id="my2" onChange={this.changeTelaState}></input>
+             <button onClick={this.changeTelaState}>test</button>
+              <p><a id="my1" href="javascript:CasteloPhyton1Button1();" >Bottom Text</a></p>
           </center>   
         );
       }

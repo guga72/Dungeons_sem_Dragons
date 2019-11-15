201c0040244c80e6d99375f0bf30ba20304706ea
Pedrofreitas18 <opedrofreitas@gmail.com>
2019-11-15 16:52:47 -0200

    react quase no jeito 15/11/19

--- public/js/CasteloPhyton1/Button1.js
+++ public/js/CasteloPhyton1/Button1.js
@@ -1,7 +1,20 @@
 //function CasteloPhyton1Button1() { alert('You clicked the bottom text'); }
 function CasteloPhyton1Button1(){
-    //alert('You clicked the bottom text'); 
+    alert('You clicked the bottom text'); 
     document.getElementById("my1").value="Trono_Castelo_Pyton-Comeco_do_jogo";
+    //return "Trono_Castelo_Pyton-Comeco_do_jogo";
     //const x = "../imagens/Castelo_Phyton_da_Colina.jpg";
 }
-
+function CasteloPhyton1Button2(){
+    //alert('You clicked the bottom text2'); 
+    document.getElementById("my1").value="Ir para sala do trono";
+    //const x = "../imagens/Castelo_Phyton_da_Colina.jpg";
+    //return "Trono_Castelo_Pyton-Comeco_do_jogo";
+    //alert('You clicked the bottom text'); 
+}
+function CasteloPhyton1Button3(){
+    //alert('You clicked the bottom text2'); 
+    //document.getElementById("my1").value="Trono_Castelo_Pyton-Comeco_do_jogo";
+    //const x = "../imagens/Castelo_Phyton_da_Colina.jpg";
+    return '"meu pau"'; 
+}

abc379f4f410b46ae856c4494fdec5a04e9b3ac7
guga72 <guga.72@hotmail.com>
2019-12-03 21:34:36 -0300

    03-12-2019 banco de dados versão final

---
+++ db/DsemD.sql
@@ -0,0 +1,31 @@
+create table usuario(
+    email varchar(255) primary key,
+    senha varchar(255)
+);
+
+create table classe(
+    id_classe int primary key,
+    nome varchar(255),
+    armas_equipaveis varchar(255)
+);
+
+create table raca(
+    id_raca int primary key,
+    nome varchar(255),
+    vida int,
+    defesa int,
+    forca int
+);
+
+create table personagem(
+    id_personagem int primary KEY,
+    gold float,
+    nivel int,
+    defesa int,
+    id_raca int,
+    id_classe int,
+    id_usuario varchar(255),
+    FOREIGN KEY(id_raca) references raca (id_raca),
+    FOREIGN KEY(id_usuario) references usuario (email),
+    FOREIGN KEY(id_classe) references classe (id_classe)
+);

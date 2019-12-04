43d222712e1047d6626fa0e3d6604a85b77cb5f8
tiagoMurer <tiagostaurenghi@hotmail.com>
2019-12-03 22:30:12 -0300

    Troca de porta do proxy

--- app.js
+++ app.js
@@ -17,3 +17,12 @@ app.use('/usuario', Usuarios)
 app.listen(port, () => {
   console.log("Server is running on port: " + port)
 })
+
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"))
+  });
+}

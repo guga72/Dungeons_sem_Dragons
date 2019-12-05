9261ceaf94f9e850123bd239e01b93a888e2f3af
guga72 <guga.72@hotmail.com>
2019-12-05 19:29:50 -0300

    mudança em alguns testes

--- client/src/gamefiles/mapa.test.js
+++ client/src/gamefiles/mapa.test.js
@@ -1,6 +1,11 @@
 const Mapa = require('./mapa.js');
 
-test('cenario deve poder ser criado', () => {
-    let cenario = Mapa.cenario("cenario", false)
-    expect(cenario.nome).toBe("cenario");
+test('certos cenario não devem poder spawnar monstros', () => {
+    let cenario = Mapa.cenario("Guilda dos Ladrões", false)
+    expect(cenario.nome).toBe("Guilda dos Ladrões");
+});
+
+test('certos cenario deve poder spawnar monstros', () => {
+    let cenario = Mapa.cenario("Estalagem Pé-Cheiroso", true)
+    expect(cenario.nome).toBe("Estalagem Pé-Cheiroso");
 });

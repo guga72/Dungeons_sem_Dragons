458f2a37d84f1a636c76913a7e5f17dc1bef0f76
guga72 <guga.72@hotmail.com>
2019-12-09 21:25:07 -0300

    mudanças no db para configuração

--- db/db.js
+++ db/db.js
@@ -2,8 +2,8 @@ const Sequelize = require("sequelize")
 
 
 const db = {}
-const sequelize = new Sequelize("DsemD", "root", "", {
-    host: 'localhost',
+const sequelize = new Sequelize("d1mqp1mgr1obg7", "dcblmxgfivlrwk", "121f619bb3fc157b86d13ef6a6a88aeddd8a23d34795c226e5322c50952425b8", {
+    host: 'ec2-54-83-36-37.compute-1.amazonaws.com',
     dialect: 'postgres',
     operatorAliases: false,
 
